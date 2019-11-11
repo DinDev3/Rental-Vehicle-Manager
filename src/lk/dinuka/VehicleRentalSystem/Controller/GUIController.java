bbff0019be9ceaca381d8e415b4afcef5b0139ec
Dinuka <drpiyadigama@gmail.com>
2019-11-11 12:50:44 +0530

    checking availability works perfectly

--- src/lk/dinuka/VehicleRentalSystem/Controller/GUIController.java
+++ src/lk/dinuka/VehicleRentalSystem/Controller/GUIController.java
@@ -5,6 +5,7 @@ import lk.dinuka.VehicleRentalSystem.Model.Schedule;
 import lk.dinuka.VehicleRentalSystem.Model.Vehicle;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class GUIController {
 
@@ -12,13 +13,11 @@ public class GUIController {
                                      int yearDropOffInput, int monthDropOffInput, int dayDropOffInput) {
         //used to create a booking as required and add booking info into the system
 
-        Date pickUpDate = new Date(yearPickUpInput, monthPickUpInput, dayPickUpInput);
-        Date dropOffDate = new Date(yearDropOffInput, monthDropOffInput, dayDropOffInput);
+        Schedule newBooking = new Schedule(yearPickUpInput, monthPickUpInput, dayPickUpInput,
+                yearDropOffInput, monthDropOffInput, dayDropOffInput);
 
-//        Schedule newBooking = new Schedule(pickUpDate, dropOffDate);
-//
-//
-//        WestminsterRentalVehicleManager.bookedVehicles.put(chosenVeh.getPlateNo(), newBooking);       //adding booked vehicle to bookedVehicles HashMap
+
+        WestminsterRentalVehicleManager.bookedVehicles.put(chosenVeh.getPlateNo(), newBooking);       //adding booked vehicle to bookedVehicles HashMap
 
         System.out.println(WestminsterRentalVehicleManager.bookedVehicles);         //checking whether required booking was entered into the system
     }
@@ -28,31 +27,40 @@ public class GUIController {
                                                  int yearDropOffInput, int monthDropOffInput, int dayDropOffInput) {
         //used to check for the availability of a chosen vehicle
 
-        Date pickUpDateCheck = new Date(yearPickUpInput, monthPickUpInput, dayPickUpInput);     //used to check pick up date entered
-        Date dropOffDateCheck = new Date(yearDropOffInput, monthDropOffInput, dayDropOffInput);     //used to check drop off date entered
+        Schedule newBooking = new Schedule(yearPickUpInput, monthPickUpInput, dayPickUpInput,       //creating new Schedule to check
+                yearDropOffInput, monthDropOffInput, dayDropOffInput);
 
+        boolean checkPickUpBefore = LocalDate.from(newBooking.getPickUp()).isBefore(        //pick up before booked pickup
+                WestminsterRentalVehicleManager.bookedVehicles.get(chosenVeh).getPickUp());
+        boolean checkDropOffBefore = LocalDate.from(newBooking.getDropOff()).isBefore(      //drop off before booked pick up
+                WestminsterRentalVehicleManager.bookedVehicles.get(chosenVeh).getDropOff());
+
+        boolean checkPickUpAfter = LocalDate.from(newBooking.getDropOff()).isAfter(                //pick up after booked drop off
+                WestminsterRentalVehicleManager.bookedVehicles.get(chosenVeh).getDropOff());
+        boolean checkDropOffAfter = LocalDate.from(newBooking.getDropOff()).isAfter(                 //drop off after booked drop off
+                WestminsterRentalVehicleManager.bookedVehicles.get(chosenVeh).getDropOff());
 
 
         if (!WestminsterRentalVehicleManager.bookedVehicles.containsKey(chosenVeh.getPlateNo())) {
             return true;        //vehicle is not booked
+        } else if ((checkPickUpBefore && checkDropOffBefore) || (checkPickUpAfter && checkDropOffAfter)) {
+            // if both pick up and drop off are either before the booked pick up date or after the booked drop off date
+
+            return true;
         }
-//        else if (WestminsterRentalVehicleManager.bookedVehicles.get(chosenVeh).getDropOff(). ||
-//                WestminsterRentalVehicleManager.bookedVehicles.get(chosenVeh).getPickUp()) {
-//            //check with before
-//        }
 
         return false;
     }
 
 
-    public static BigDecimal getCalculatedRent(BigDecimal dailyCost){
+    public static BigDecimal getCalculatedRent(BigDecimal dailyCost) {
 //        have calculation of total cost here
 
-        BigDecimal totalCost= BigDecimal.valueOf(0);
+        BigDecimal totalCost = BigDecimal.valueOf(0);
         int noOfDays = 0;
 
 
-        if (noOfDays>0){
+        if (noOfDays > 0) {
             return dailyCost.multiply(BigDecimal.valueOf(noOfDays));    //dailyCost*noOfDays
         }
         return totalCost;
